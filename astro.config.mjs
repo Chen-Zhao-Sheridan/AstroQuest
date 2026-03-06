06cb03074c82f3611678fab71807165bb7407401
Chen Zhao <zhao58@sheridancollege.ca>
2026-03-05 22:04:39 -0500

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Replace with your GitHub username
+  site: 'https://Chen-Zhao-Sheridan.github.io', 
+  // Replace with your repository name (starting with a slash)
+  base: '/AstroQuest', 
+});
