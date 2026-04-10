88853357ce9dd4e76d28c3ee5cf6e08cbee57267
Matthew <ilovelego@outlook.com>
2026-04-10 16:21:13 -0400

    move map settings into map.astro, update map.js with new controls

---
+++ src/js/map.js
@@ -0,0 +1,32 @@
+import { createLightPollutionOverlay } from "/src/lib/LightPollutionOverlay.ts";
+// Wait for the <gmp-map> custom element to be fully upgraded
+await customElements.whenDefined("gmp-map");
+
+const mapEl = document.querySelector("gmp-map");
+
+// Method A: grab innerMap directly
+const map = mapEl.innerMap;
+
+const lpoverlay = createLightPollutionOverlay(0.6);
+lpoverlay.setMap(map);
+
+// Expose globally so other scripts/islands can control it
+window.lightPollutionOverlay = lpoverlay;
+
+// Controls
+mapSettingsReveal = document.getElementById("map-settings-reveal")
+mapSettingsHide = document.getElementById("map-settings-hide")
+mapSettings = document.getElementById("map-settings")
+mapSettingsReveal.addEventListener("click", () =>{
+    mapSettings.hidden = false
+})
+mapSettingsHide.addEventListener("click", () => {
+    mapSettings.hidden = true
+})
+
+overlayDropdown = document
+    .getElementById("overlay-select")
+overlayDropdown.addEventListener("change", () => {
+    overlayDropdown.value === 'light-pollution-overlay' ? lpoverlay.show() : lpoverlay.hide()
+});
+
