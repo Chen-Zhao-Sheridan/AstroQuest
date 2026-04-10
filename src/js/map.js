b925a4c431c342291ca97dc1aa76bfd2ff88705d
Matthew <ilovelego@outlook.com>
2026-04-10 17:04:32 -0400

    hide overlay on load

--- src/js/map.js
+++ src/js/map.js
@@ -9,15 +9,12 @@ const map = mapEl.innerMap;
 
 const lpoverlay = createLightPollutionOverlay(0.6);
 lpoverlay.setMap(map);
+lpoverlay.hide();
 
 // Expose globally so other scripts/islands can control it
 window.lightPollutionOverlay = lpoverlay;
 
 // Controls
-document.addEventListener("DOMContentLoaded", function () {
-    lpoverlay.hide()
-});
-
 var mapSettingsReveal = document.getElementById("map-settings-reveal");
 var mapSettingsHide = document.getElementById("map-settings-hide");
 var mapMain = document.getElementById("map-main");
@@ -32,7 +29,7 @@ mapSettingsHide.addEventListener("click", () => {
 });
 
 var lpOverlayToggle = document.getElementById("lp-overlay-toggle");
-let visible = true;
+let visible = false;
 lpOverlayToggle.addEventListener("click", () => {
     visible = !visible;
     visible ? lpoverlay.show() : lpoverlay.hide();
